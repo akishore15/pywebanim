9585eeb5192b72c9206aca48cff191f420eb2cea
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2026-03-26 22:06:04 -0700

    Update cli.py

--- pywebanim/cli.py
+++ pywebanim/cli.py
@@ -1,3 +1,14 @@
+"""
+pywebanim - Embed Manim animations into HTML
+Copyright (C) 2026 Some Person
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY.
+"""
 import argparse
 from .renderer import render_scene
 from .html import generate_html
